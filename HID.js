34a322f01f8a89ea5b1ffa3f6debdc64b261e4ec
illuminarias <illuminarias96@gmail.com>
2020-08-21 10:58:51 -0700

    fix code for HSV

--- HID.js
+++ HID.js
@@ -1,26 +1,25 @@
 var HID = require('node-hid');	//required
-var vid = 0x6F6C;	//VID for planck
-var pid = 0x706C;	//PID for planck
+var vid = 0xFEED;	//VID for planck
+var pid = 0x6060;	//PID for planck
 var usagePage = 0xFF60;	//usagePage for planck. dumped from HID.devices()
 var usage = 0x61;	//usage for planck. dumped from HID.devices();
 var keyboard = null;	//keyboard object. null at start.
 
-
-
-
-
 connectKeyboard();
-
-	for(i = 0; i < 10; i++){
-		keyboard.write([0,i]);
-
-	}
-
-keyboard.on('data', function(data) {
-	console.log(data);
-});
+writeKeyboard();
+readKeyboard();
 
 
+function readKeyboard(){
+	keyboard.on('data', function(data) {
+		console.log(data);
+		keyboard.close();
+	});
+}
+
+function writeKeyboard(){
+	keyboard.write([0,2,50,255,255]);
+}
 
 function connectKeyboard(){
 	var devices = HID.devices();	//get all HID devices instead of connecting directly with PID and VID as some devices will have multiple HID "channels(?)"
