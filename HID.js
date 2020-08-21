fed9fdd79e65a74cba2bec20335cf474ccff83be
illuminarias <illuminarias96@gmail.com>
2020-08-21 11:00:09 -0700

    change variable scope to reduce chances of leak and unused variables

--- HID.js
+++ HID.js
@@ -1,9 +1,6 @@
 var HID = require('node-hid');	//required
-var vid = 0xFEED;	//VID for planck
-var pid = 0x6060;	//PID for planck
-var usagePage = 0xFF60;	//usagePage for planck. dumped from HID.devices()
-var usage = 0x61;	//usage for planck. dumped from HID.devices();
 var keyboard = null;	//keyboard object. null at start.
+
 
 connectKeyboard();
 writeKeyboard();
@@ -18,10 +15,14 @@ function readKeyboard(){
 }
 
 function writeKeyboard(){
-	keyboard.write([0,2,50,255,255]);
+	keyboard.write([0,2,50,255,255]);	//0, "hid chooser", "values".....
 }
 
 function connectKeyboard(){
+	var vid = 0xFEED;	//VID for planck
+	var pid = 0x6060;	//PID for planck
+	var usagePage = 0xFF60;	//usagePage for planck. dumped from HID.devices()
+	var usage = 0x61;	//usage for planck. dumped from HID.devices();
 	var devices = HID.devices();	//get all HID devices instead of connecting directly with PID and VID as some devices will have multiple HID "channels(?)"
 	var deviceInfo = devices.find( function(d) {	
     var isPlanck = d.vendorId===vid && d.productId===pid;	//make sure eveyrthing matches
